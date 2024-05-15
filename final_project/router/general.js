9db76e362ffe721b1f70e6dcb6680246193b97d0
MartinsKristops <166022550+MartinsKristops@users.noreply.github.com>
2024-05-15 10:22:43 +0300

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,26 +6,29 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
+  res.send(users);
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
+  res.send(books);
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/isbn/:isbn',async function (req, res) {
+    const isbn = req.params.isbn;
+    let filtered_books = books.filter((book) => book.isbn === isbn);
+    res.send(filtered_books);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.author;
+    let author_filtered = books.filter((book) => book.author === author);
+    res.send(author_filtered);
+  
 });
 
 // Get all books based on title
